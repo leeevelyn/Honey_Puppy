ecc3bb48c2f7905599f48a93225d5bb15aa68fe1
Eric Hong <eric@guest-wireless-207-151-035-004.usc.edu>
2014-11-08 17:14:35 -0800

    added endgame

--- Honey_Puppy/Assets/Scripts/Player.cs
+++ Honey_Puppy/Assets/Scripts/Player.cs
@@ -14,12 +14,13 @@ public class Player : MonoBehaviour {
 	/// <summary>
 	/// If the player is hit with a dodgeball, kill him and restart the scene.
 	/// </summary>
-	/*private void OnCollisionEnter(Collision other) {
+	private void OnCollisionEnter(Collision other) {
 		if(other.gameObject.name == "Dodgeball(Clone)") {
-			SceneFade other_script = (SceneFade) screenFader.GetComponent(typeof(SceneFade));
-			other_script.EndScene();
+			/*SceneFade other_script = (SceneFade) screenFader.GetComponent(typeof(SceneFade));
+			other_script.EndScene();*/
+			Application.LoadLevel("endscreen");
 		}
-	}*/
+	}
 	public int speed;
 
 	void Update(){
---
+++ Honey_Puppy/Assets/Scripts/endmenu.js
@@ -0,0 +1,22 @@
+﻿var isMenu=false;
+
+function OnMouseEnter(){
+	//change text color
+	renderer.material.color=Color.black;
+}
+
+function OnMouseExit(){
+	//change text color
+	renderer.material.color=Color.white;
+}
+
+function OnMouseUp(){
+	Application.Quit();
+}
+
+function Update(){
+	//quit game if escape key is pressed
+	if (Input.GetKey(KeyCode.Escape)) { 
+		Application.Quit();
+	}
+}
